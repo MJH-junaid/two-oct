25df6c8b4fea404e3c9fa126c774f554660fc849
MohammadJunaidG <mojunaid2050@gmail.com>
2025-10-02 17:38:38 +0530

    change-location

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const images = ['1.jpg', '2.jpg', '3.jpg','4.jpg','5.jpg']
+const images = ['/img/1.jpg', '/img/2.jpg', '/img/3.jpg','/img/4.jpg','/img/5.jpg']
 
 let currentIndex = 0;
 const sliderImage = document.getElementById('slider-image')
